289c51b2b90dde9cd818d056c4a5e4988077cea0
Ebin Varghese <ebyblr2k18@gmail.com>
2022-09-30 13:48:30 +0530

    cards added_home_page

--- script.js
+++
@@ -1,6 +0,0 @@
-
-var menuIcon = document.querySelector(".menuicon");
-var sideBar = document.querySelector(".sidebar");
-menuIcon.onclick = function(){
-    sideBar.classList.toggle("small-sidebar")
-}
